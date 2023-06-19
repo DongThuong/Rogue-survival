6f793262146d4124841169fc5b9f31fb551c3613
DongThuong <113157828+DongThuong@users.noreply.github.com>
2023-06-19 16:31:22 +0700

    Initial commit

---
+++ Assets/Resources/Scripts/Bullet/BulletController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    private EnemyHealth enemyHealth;
+    public int damage;
+    private GameObject explosion;
+
+    void Start() 
+    {
+        Destroy(gameObject, 4f);
+        explosion = Resources.Load<GameObject>("Prefabs/Effects/BulletExplosionEffect1");
+    }
+    
+    // su ken khi va cham voi cac doi tuong khac
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+
+        // khi cham vao object tag Enemy
+        if (other.CompareTag("Enemy")) 
+        {
+            enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
+            enemyHealth.decreaseHealth(damage);
+        }
+        // khi cham vao object tag Boss
+        if (other.CompareTag("Boss")) 
+        {
+            BossHealth bossHealth = other.GetComponent<BossHealth>();
+            bossHealth.TakeDamage(damage);
+        }
+        //khi cham vao object tag Hydra
+        if (other.CompareTag("Hydra")) 
+        {
+            HydraHealth bossHealth = other.GetComponent<HydraHealth>();
+            bossHealth.TakeDamage(damage);
+        }
+        // k cham cac tag duoi thi pha huy va sinh ra hieu ung
+        if (!other.CompareTag("Player") &&
+            !other.CompareTag("PlayerChild") &&
+            !other.CompareTag("Bullet") &&
+            !other.CompareTag("Hole") &&
+            !other.CompareTag("EnemyBullet") &&
+            !other.CompareTag("Ignore")) 
+        {
+            Destroy(gameObject); // pha huy
+            Instantiate(explosion, transform.position, transform.rotation); // sinh ra hieu ung
+            SoundManager.PlaySound("Explosion"); // phat am thanh hieu ung
+        }
+    }
+
+    public int Damage 
+    {
+        get { return damage; }
+    }
+}
---
+++ Assets/Resources/Scripts/PlayerAbilities/ActivateRadialAttack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ActivateRadialAttack : MonoBehaviour {
+    public GameObject bullet;
+    public Transform shootPoint;
+    private bool wait;
+    public float waitTime;
+    
+    void Start() 
+    {
+        wait = true;
+    }
+
+    void Update() 
+    {
+        if (Input.GetButtonDown("Fire2") && wait && Time.timeScale != 0) 
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack() 
+    {
+        wait = false;
+
+        for (int i = 0; i <= 360; i += 20) 
+        {
+            shootPoint.transform.rotation = Quaternion.Euler(0, 0, i); // goc ban ra dan
+
+            // sinh ra dan
+            GameObject bulletInstantiated = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+            Rigidbody2D bulletRB = bulletInstantiated.GetComponent<Rigidbody2D>();
+
+            // them luc cho dan
+            bulletRB.AddForce(shootPoint.right * 15, ForceMode2D.Impulse);
+            SoundManager.PlaySound("EnergyShot"); // phat am thanh nkhi ban
+            yield return new WaitForSeconds(0.025f);
+        }
+
+        yield return new WaitForSeconds(waitTime);
+        wait = true;
+    }
+}
